fd5bdaaf824b09b1119debed349165abc40f1418
Swarnim08 <52878084+Swarnim08@users.noreply.github.com>
2022-05-03 17:16:04 +0530

    Update streamlit_app.py
    Added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale, Spinach and Rocket Smoothie')
+streamlit.text('hard-Boiled Free-Range Egg')
