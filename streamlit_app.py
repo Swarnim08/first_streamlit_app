35b5dcb9ef0a6c4f0a1d462e9e1095061e2e768b
Swarnim08 <52878084+Swarnim08@users.noreply.github.com>
2022-05-07 19:57:49 +0530

    Update streamlit_app.py
    adding some def function and returning the data

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,13 +47,22 @@ streamlit.write('The user entered', fruit_choice)
 #don't run anything past here while we troubleshoot
 streamlit.stop()
 
-#import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#import snowflake.connector
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_lsit")
+    return my_car.fetchall()
+
+#add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+ 
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT * from fruit_load_list")
+#streamlit.header("The fruit load list contains:")
 
 fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding', fruit_choice)
