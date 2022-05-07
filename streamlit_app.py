f69cd5595d3cd8849e45beb630364a79935b7e5d
Swarnim08 <52878084+Swarnim08@users.noreply.github.com>
2022-05-07 19:58:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ streamlit.write('The user entered', fruit_choice)
 #streamlit.dataframe(fruityvice_normalized)
 
 #don't run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 #import snowflake.connector
